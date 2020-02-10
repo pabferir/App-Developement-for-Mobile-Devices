ae971ce0966e664c70aa8b387c7a84b8bbb60ee4
Pablo <pablo_f.i@hotmail.com>
2020-02-10 17:29:36 +0100

    Quotation Activity completed

--- app/src/main/java/com/pabferir/dashboardactivity/QuotationActivity.java
+++ app/src/main/java/com/pabferir/dashboardactivity/QuotationActivity.java
@@ -3,6 +3,8 @@ package com.pabferir.dashboardactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class QuotationActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class QuotationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation);
+
+        TextView quotationView = (TextView)findViewById(R.id.quotation_quote_tv);
+        String quote = getResources().getString(R.string.quotes_label_tv1,
+                getResources().getString(R.string.user_defaultName));
+
+        quotationView.setText(quote);
+    }
+
+    public void newQuotation(View v) {
+        TextView quotationView = (TextView)findViewById(R.id.quotation_quote_tv);
+        quotationView.setText(R.string.quotes_label_sampleQuote);
+
+        TextView authorView = (TextView) findViewById(R.id.quotation_author_tv);
+        authorView.setText(R.string.quotes_label_sampleAuthor);
     }
 }
