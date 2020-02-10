a01590f4a9af82a13ad8e8f9a97731decac0a208
Pablo <pablo_f.i@hotmail.com>
2020-02-10 16:37:01 +0100

    Favourite Activity completed

--- app/src/main/java/com/pabferir/dashboardactivity/FavouriteActivity.java
+++ app/src/main/java/com/pabferir/dashboardactivity/FavouriteActivity.java
@@ -2,7 +2,10 @@ package com.pabferir.dashboardactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class FavouriteActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class FavouriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
+    }
+
+    public void getAuthorInfo(View v) {
+        String authorName = "Albert Einstein";
+
+        Uri wikipedia = Uri.parse("https://en.wikipedia.org/wiki/Special:Search?search=" + authorName);
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, wikipedia);
+        startActivity(webIntent);
     }
 }
